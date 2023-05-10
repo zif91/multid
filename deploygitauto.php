ad25dfd4362cc68af1b52bfbe09a4b9bfe43b5ac
zif91 <65787947+zif91@users.noreply.github.com>
2023-05-11 00:05:51 +0200

    Update deploygitauto.php

--- deploygitauto.php
+++ deploygitauto.php
@@ -1,5 +1,12 @@
 <?php
+$result = exec('whoami');
+var_dump($result);
+echo "<br>";
 $dir = 'jacov9sw.beget.tech/public_html';
+exec("git config --global safe.directory '*' && git pull 2>&1", $output);
+var_dump($output);
+
 exec("cd $dir && git pull 2>&1", $output);
-print_r($output);
-//test autoload  
+var_dump($output);
+
+//test autoload  123

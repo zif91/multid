352dfb65b30da2c8e53cb324f76cf6d871201afc
zif91 <65787947+zif91@users.noreply.github.com>
2023-05-11 22:23:53 +0200

    Update deploygitauto.php

--- deploygitauto.php
+++ deploygitauto.php
@@ -4,11 +4,13 @@ $dir = '/home/jacov9sw.beget.tech/public_html';
 try {
     exec("cd $dir && git pull 2>&1", $output);
     Log::info($output);
+    var_dump($output);
 } catch (\Exception $e) {
     Log::error($e);
+    var_dump($e);
 }
-
 http_response_code(200);
+
 
 var_dump($output);
 var_dump($e);

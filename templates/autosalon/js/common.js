0afba3681a7ed6f939aab0568e50a6928e020718
zif91 <65787947+zif91@users.noreply.github.com>
2023-05-12 19:17:24 +0200

    sc

--- templates/autosalon/js/common.js
+++ templates/autosalon/js/common.js
@@ -9,6 +9,20 @@ $(function(){
 	  arrows: false,
 	  asNavFor: '.slider-nav'
 	});
+	window.showCreditPopup = function(e, t, i) {
+                $("#credit-popup-plate .popup__image").attr("src", ""),
+                $.fancybox.open({
+                    src: "#credit-popup-plate",
+                    type: "inline",
+                    opts: {
+                        beforeShow: function(n, s) {
+                            $("#credit-popup-plate .popup__car_id").val(e),
+                            $("#credit-popup-plate .popup__image").attr("src", t),
+                            $("#credit-popup-plate .popup__price").text(i + " руб/мес")
+                        }
+                    }
+                })
+            };
 	$('.slider-nav').slick({
 	  slidesToShow: 6,
 	  slidesToScroll: 1,

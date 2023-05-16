baca4df3ce3f8b8ff7de1d2725262e35ea20af86
zif91 <65787947+zif91@users.noreply.github.com>
2023-05-17 00:27:43 +0200

    Update common.js

--- templates/autosalon/js/common.js
+++ templates/autosalon/js/common.js
@@ -1,53 +1,56 @@
-$(function(){
+$(function() {
 	$('.index-brands-block .bottom-link a').click(function() {
 		$(this).parent().hide();
 		$('.index-brands-block .row .col-3').show();
 	});
 	$('.slider-for').slick({
-	  slidesToShow: 1,
-	  slidesToScroll: 1,
-	  arrows: false,
-	  asNavFor: '.slider-nav'
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		arrows: false,
+		asNavFor: '.slider-nav'
 	});
 	$(".bottom-page-line .close").click(function() {
 		$(".bottom-page-line").hide();
 	});
 	$(".bottom-page-line").click(function() {
-      $.fancybox.open("trade-in-popup");
-    });
+		$.fancybox.open({
+			src: '#trade-in-popup',
+			type: 'inline'
+		});
+	});
+
 	window.showCreditPopup = function(e, t, i) {
-                $("#credit-popup-plate .popup__image").attr("src", ""),
-                $.fancybox.open({
-                    src: "#credit-popup-plate",
-                    type: "inline",
-                    opts: {
-                        beforeShow: function(n, s) {
-                            $("#credit-popup-plate .popup__car_id").val(e),
-                            $("#credit-popup-plate .popup__image").attr("src", t),
-                            $("#credit-popup-plate .popup__price").text(i + " руб/мес")
-                        }
-                    }
-                })
-            };
+		$("#credit-popup-plate .popup__image").attr("src", ""),
+			$.fancybox.open({
+				src: "#credit-popup-plate",
+				type: "inline",
+				opts: {
+					beforeShow: function(n, s) {
+						$("#credit-popup-plate .popup__car_id").val(e),
+							$("#credit-popup-plate .popup__image").attr("src", t),
+							$("#credit-popup-plate .popup__price").text(i + " руб/мес")
+					}
+				}
+			})
+	};
 	$('.slider-nav').slick({
-	  slidesToShow: 6,
-	  slidesToScroll: 1,
-	  asNavFor: '.slider-for',
-	  arrows: false,
-	  focusOnSelect: true,
-	  responsive: [
-	      {
-	        breakpoint: 1200,
-	        settings: {
-	          slidesToShow: 5
-	        }
-	      },
-	      {
-	        breakpoint: 768,
-	        settings: {
-	          slidesToShow: 4
-	        }
-	      }
-	    ]
+		slidesToShow: 6,
+		slidesToScroll: 1,
+		asNavFor: '.slider-for',
+		arrows: false,
+		focusOnSelect: true,
+		responsive: [{
+				breakpoint: 1200,
+				settings: {
+					slidesToShow: 5
+				}
+			},
+			{
+				breakpoint: 768,
+				settings: {
+					slidesToShow: 4
+				}
+			}
+		]
 	});
 });

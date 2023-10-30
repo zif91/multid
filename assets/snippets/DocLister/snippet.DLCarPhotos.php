c5d90a1989e634c8ce6d2dd6028a55431376e8fb
hkyss <hkyss.services@protonmail.com>
2023-10-30 22:11:45 +0100

    fix: car photos

--- assets/snippets/DocLister/snippet.DLCarPhotos.php
+++ assets/snippets/DocLister/snippet.DLCarPhotos.php
@@ -1,9 +1,9 @@
 <?php
 $data['car_photos'] = json_decode($data['tv.car_photos'], true);
 
-if (!empty($data['car_photos'])) {
+if (!empty($data['car_photos']) && !empty($data['car_photos']['fieldValue'])) {
     $data['photos'] = [];
-    foreach ($data['car_photos'] as $photo) {
+    foreach ($data['car_photos']['fieldValue'] as $photo) {
         $data['photos'][] = $photo['image'];
     }
     $data['photo'] = $data['photos'][0];

591bce6ef1b395d13c20afee71ee03c3659eddf8
hkyss <hkyss.services@protonmail.com>
2024-02-26 22:41:00 +0300

    fix: all forms to the one tab

--- assets/modules/formresults/config/FormTradeIn.php
+++ assets/modules/formresults/config/FormTradeIn.php
@@ -1,29 +1,29 @@
 <?php
 
-return [
-    'caption' => 'Заявки TradeIn',
-
-    'fields' => [
-        'first_name' => [
-            'caption' => 'ФИО',
-        ],
-        'mobile_tel' => [
-            'caption' => 'Телефон',
-        ],
-        'url' => [
-            'caption' => 'URL',
-        ],
-        'utm_source' => [
-            'caption' => 'utm_source',
-        ],
-        'utm_medium' => [
-            'caption' => 'utm_medium',
-        ],
-        'utm_content' => [
-            'caption' => 'utm_content',
-        ],
-        'utm_campaign' => [
-            'caption' => 'utm_campaign',
-        ],
-    ],
-];
+//return [
+//    'caption' => 'Заявки TradeIn',
+//
+//    'fields' => [
+//        'first_name' => [
+//            'caption' => 'ФИО',
+//        ],
+//        'mobile_tel' => [
+//            'caption' => 'Телефон',
+//        ],
+//        'url' => [
+//            'caption' => 'URL',
+//        ],
+//        'utm_source' => [
+//            'caption' => 'utm_source',
+//        ],
+//        'utm_medium' => [
+//            'caption' => 'utm_medium',
+//        ],
+//        'utm_content' => [
+//            'caption' => 'utm_content',
+//        ],
+//        'utm_campaign' => [
+//            'caption' => 'utm_campaign',
+//        ],
+//    ],
+//];
---
+++ assets/modules/formresults/config/leads.php
@@ -0,0 +1,29 @@
+<?php
+
+return [
+    'caption' => 'Все лиды',
+
+    'fields' => [
+        'first_name' => [
+            'caption' => 'ФИО',
+        ],
+        'mobile_tel' => [
+            'caption' => 'Телефон',
+        ],
+        'url' => [
+            'caption' => 'URL',
+        ],
+        'utm_source' => [
+            'caption' => 'utm_source',
+        ],
+        'utm_medium' => [
+            'caption' => 'utm_medium',
+        ],
+        'utm_content' => [
+            'caption' => 'utm_content',
+        ],
+        'utm_campaign' => [
+            'caption' => 'utm_campaign',
+        ],
+    ],
+];

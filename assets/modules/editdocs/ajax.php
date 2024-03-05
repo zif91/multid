7e13d58dfc5225b3d7df8e0266d56a5915ebd21e
hkyss <hkyss.services@protonmail.com>
2024-03-05 21:43:14 +0300

    fix: editdocs to php8

--- assets/modules/editdocs/ajax.php
+++ assets/modules/editdocs/ajax.php
@@ -22,7 +22,7 @@ if (IN_MANAGER_MODE != "true" || empty($modx) || !($modx instanceof DocumentPars
 if (!$modx->hasPermission('exec_module')) {
     header("location: " . $modx->getManagerPath() . "?a=106");
 }
-if (!is_array($modx->event->params)) {
+if (!empty($modx->event->params) && !is_array($modx->event->params)) {
     $modx->event->params = array();
 }
 if (!isset($_SESSION['mgrValidated'])) {
